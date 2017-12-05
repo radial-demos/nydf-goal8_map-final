b6c21b2948a16c2ab9ce7b679520550b9a9dca50
Darren Anderson <dranders@colorado.ed>
2017-12-05 09:12:15 -0800

    Create framework for React app, making sure bundling is working.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Map from './map.jsx';
+
+ReactDOM.render(
+  <Map/>,
+  document.getElementById('goal8-map'),
+);
---
+++ src/map.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>I'm going to be a cool map when I grow up.</div>
+    );
+  }
+}
+
+export default Component;
