dd2b452b2d7706a298ce8fae0d4c274c8c136f8f
Darren Anderson <dranders@colorado.ed>
2017-12-05 10:46:46 -0800

    Include configuration files for country names and coordinates keyed on ISO A2 country code.

---
+++ config/country-names.js
@@ -0,0 +1,200 @@
+const name = {};
+
+name.AF = 'Afghanistan';
+name.AL = 'Albania';
+name.DZ = 'Algeria';
+name.AO = 'Angola';
+name.AR = 'Argentina';
+name.AM = 'Armenia';
+name.AU = 'Australia';
+name.AT = 'Austria';
+name.AZ = 'Azerbaijan';
+name.BH = 'Bahrain';
+name.BD = 'Bangladesh';
+name.BY = 'Belarus';
+name.BE = 'Belgium';
+name.BJ = 'Benin';
+name.BT = 'Bhutan';
+name.BO = 'Bolivia';
+name.BA = 'Bosnia and Herzegovina';
+name.BW = 'Botswana';
+name.BR = 'Brazil';
+name.BN = 'Brunei';
+name.BG = 'Bulgaria';
+name.BF = 'Burkina Faso';
+name.BI = 'Burundi';
+name.KH = 'Cambodia';
+name.CM = 'Cameroon';
+name.CA = 'Canada';
+name.CV = 'Cape Verde';
+name.CF = 'Central African Rep.';
+name.TD = 'Chad';
+name.CL = 'Chile';
+name.CN = 'China';
+name.CO = 'Colombia';
+name.KM = 'Comoros';
+name.CD = 'Congo, Dem. Rep.';
+name.CG = 'Congo, Rep.';
+name.CR = 'Costa Rica';
+name.CI = 'Cote d\'Ivoire';
+name.HR = 'Croatia';
+name.CU = 'Cuba';
+name.CY = 'Cyprus';
+name.CZ = 'Czech Rep.';
+name.DK = 'Denmark';
+name.DJ = 'Djibouti';
+name.DO = 'Dominican Rep.';
+name.EC = 'Ecuador';
+name.EG = 'Egypt';
+name.SV = 'El Salvador';
+name.GQ = 'Equatorial Guinea';
+name.ER = 'Eritrea';
+name.EE = 'Estonia';
+name.ET = 'Ethiopia';
+name.FJ = 'Fiji';
+name.FI = 'Finland';
+name.FR = 'France';
+name.GA = 'Gabon';
+name.GM = 'Gambia';
+name.GE = 'Georgia';
+name.DE = 'Germany';
+name.GH = 'Ghana';
+name.GR = 'Greece';
+name.GT = 'Guatemala';
+name.GN = 'Guinea';
+name.GW = 'Guinea-Bissau';
+name.GY = 'Guyana';
+name.HT = 'Haiti';
+name.HN = 'Honduras';
+name.HK = 'Hong Kong, China';
+name.HU = 'Hungary';
+name.IS = 'Iceland';
+name.IN = 'India';
+name.ID = 'Indonesia';
+name.IR = 'Iran';
+name.IQ = 'Iraq';
+name.IE = 'Ireland';
+name.IL = 'Israel';
+name.IT = 'Italy';
+name.JM = 'Jamaica';
+name.JP = 'Japan';
+name.JO = 'Jordan';
+name.KZ = 'Kazakhstan';
+name.KE = 'Kenya';
+name.KP = 'Korea, Dem. Rep.';
+name.KR = 'Korea, Rep.';
+name.KW = 'Kuwait';
+name.KG = 'Kyrgyzstan';
+name.LA = 'Laos';
+name.LV = 'Latvia';
+name.LB = 'Lebanon';
+name.LS = 'Lesotho';
+name.LR = 'Liberia';
+name.LY = 'Libya';
+name.LT = 'Lithuania';
+name.LU = 'Luxembourg';
+name.MK = 'Macedonia, FYR';
+name.MG = 'Madagascar';
+name.MW = 'Malawi';
+name.MY = 'Malaysia';
+name.ML = 'Mali';
+name.MR = 'Mauritania';
+name.MU = 'Mauritius';
+name.MX = 'Mexico';
+name.MD = 'Moldova';
+name.MN = 'Mongolia';
+name.ME = 'Montenegro';
+name.MA = 'Morocco';
+name.MZ = 'Mozambique';
+name.MM = 'Myanmar';
+name.NA = 'Namibia';
+name.NP = 'Nepal';
+name.NL = 'Netherlands';
+name.NZ = 'New Zealand';
+name.NI = 'Nicaragua';
+name.NE = 'Niger';
+name.NG = 'Nigeria';
+name.NO = 'Norway';
+name.OM = 'Oman';
+name.PK = 'Pakistan';
+name.PA = 'Panama';
+name.PG = 'Papua New Guinea';
+name.PY = 'Paraguay';
+name.PE = 'Peru';
+name.PH = 'Philippines';
+name.PL = 'Poland';
+name.PT = 'Portugal';
+name.PR = 'Puerto Rico';
+name.QA = 'Qatar';
+name.RO = 'Romania';
+name.RU = 'Russia';
+name.RW = 'Rwanda';
+name.SA = 'Saudi Arabia';
+name.SN = 'Senegal';
+name.RS = 'Serbia';
+name.SL = 'Sierra Leone';
+name.SG = 'Singapore';
+name.SK = 'Slovak Republic';
+name.SI = 'Slovenia';
+name.SB = 'Solomon Islands';
+name.SO = 'Somalia';
+name.ZA = 'South Africa';
+name.ES = 'Spain';
+name.LK = 'Sri Lanka';
+name.SD = 'Sudan';
+name.SR = 'Suriname';
+name.SZ = 'Swaziland';
+name.SE = 'Sweden';
+name.CH = 'Switzerland';
+name.SY = 'Syria';
+name.TW = 'Taiwan';
+name.TJ = 'Tajikistan';
+name.TZ = 'Tanzania';
+name.TH = 'Thailand';
+name.TG = 'Togo';
+name.TT = 'Trinidad and Tobago';
+name.TN = 'Tunisia';
+name.TR = 'Turkey';
+name.TM = 'Turkmenistan';
+name.UG = 'Uganda';
+name.UA = 'Ukraine';
+name.AE = 'United Arab Emirates';
+name.GB = 'United Kingdom';
+name.US = 'United States';
+name.UY = 'Uruguay';
+name.UZ = 'Uzbekistan';
+name.VE = 'Venezuela';
+name.PS = 'West Bank and Gaza';
+name.VN = 'Vietnam';
+name.YE = 'Yemen, Rep.';
+name.ZM = 'Zambia';
+name.ZW = 'Zimbabwe';
+// Overrides for NYDF-supplied country names
+// Andorra
+// Antigua and Barbuda
+// Bahamas
+// Barbados
+// Belize
+name.CF = 'Central African Republic';
+name.CD = 'Democratic Republic of Congo';
+// Dominica
+name.DO = 'Dominican Republic';
+// Grenada
+// Macedonia
+// Maldives
+// Micronesia
+name.KP = 'North Korea';
+// Palau
+// Palestine
+name.CG = 'Republic of Congo';
+// Saint Kitts and Nevis
+// Saint Lucia
+// Saint Vincent and the Grenadines
+// San Marino
+// Seychelles
+name.KR = 'South Korea';
+// South Sudan
+// Timor-Leste
+// Vanuatu
+
+module.exports = name;
