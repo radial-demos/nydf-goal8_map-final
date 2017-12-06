9d66c909e8ca24be625df5c7eab23adb2132e5fd
Darren Anderson <dranders@colorado.ed>
2017-12-06 08:25:15 -0800

    Set up env global for debug support. Require fieldDefinitions and pass as prop to Map component.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import Map from './map.jsx';
+import fieldDefinitions from '../config/field-definitions';
+
+process.env.DEBUG = 'nydf:*';
 
 ReactDOM.render(
-  <Map/>,
+  <Map
+    fieldDefinitions={fieldDefinitions}
+  />,
   document.getElementById('goal8-map'),
 );
--- src/map.jsx
+++ src/map.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+const debug = require('debug')('nydf:map');
+
 class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    this.forestFields = props.fieldDefinitions.filter(d => d.type === 'forest');
+    this.financeFields = props.fieldDefinitions.filter(d => d.type === 'finance');
+    // this.state = { activeId: props.items[0].id };
+  }
+
   render() {
+    debug(this.forestFields);
     return (
-      <div>I'm going to be a cool map when I grow up.</div>
+      <div>I'm going to be a cool map when I grow up</div>
     );
   }
 }
