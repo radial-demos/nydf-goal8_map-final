89be1de2c66fdde1225ce580277bc080976ae0c2
Darren Anderson <dranders@colorado.ed>
2017-12-06 08:59:24 -0800

    Add click handler to NavRow component.

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,16 +4,27 @@ const debug = require('debug')('nydf:nav');
 
 class NavRow extends React.Component {
   constructor(props) {
-  super(props);
-  // this.forestFields = props.fieldDefinitions.filter(d => d.type === 'forest');
-  // this.financeFields = props.fieldDefinitions.filter(d => d.type === 'finance');
-  // this.state = { activeId: props.items[0].id };
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    // this.forestFields = props.fieldDefinitions.filter(d => d.type === 'forest');
+    // this.financeFields = props.fieldDefinitions.filter(d => d.type === 'finance');
+    // this.state = { activeId: props.items[0].id };
+  }
+
+  handleClick(fieldType, fieldId) {
+    debug(fieldType, fieldId);
   }
 
   render() {
     return (
       <div>
-        {this.props.fields.map(field => <span key={field.id}>{field.label}</span>)}
+        {this.props.fields.map(field =>
+          <span
+            key={field.id}
+            onClick={this.handleClick.bind(this, field.type, field.id)}
+          >
+          {field.label}
+          </span>)}
       </div>
     );
   }
