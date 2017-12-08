318099c40bc92265b38a61235f2017ce6a80453e
Darren Anderson <dranders@colorado.ed>
2017-12-08 11:25:19 -0800

    Refactor for single class attribute 'availableFields' having keys/values for 'forest' and 'finance' instead of two separate class attributes. For the state variable 'activeFields', use full field definitions instead of just the IDs of the active 'forest' and 'finance' field.

--- src/map.jsx
+++ src/map.jsx
@@ -1,29 +1,39 @@
 import React from 'react';
+import { orderBy } from 'lodash';
 
 import Nav from './components/nav.jsx';
 import Legend from './components/legend.jsx';
+import Table from './components/table.jsx';
 
 const debug = require('debug')('nydf:map');
 
 class Component extends React.Component {
   constructor(props) {
     super(props);
-    this.forestFields = props.fieldDefinitions.filter(d => d.type === 'forest');
-    this.financeFields = props.fieldDefinitions.filter(d => d.type === 'finance');
+    this.availableFields = {
+      forest: props.fieldDefinitions.filter(d => d.type === 'forest').map(d => Object.assign(d, { indexes: getIndexes(d) })),
+      finance: props.fieldDefinitions.filter(d => d.type === 'finance').map(d => Object.assign(d, { indexes: getIndexes(d) })),
+    };
+    // state initialization
     this.state = {
       activeFields: {
-        forest: this.forestFields[0].id,
-        finance: this.financeFields[0].id,
+        forest: this.availableFields.forest[0],
+        finance: this.availableFields.finance[0],
       },
     };
+    // function bindings
     this.updateActiveFields = this.updateActiveFields.bind(this);
-    // this.state = { activeId: props.items[0].id };
+    // constructor support functions
+    function getIndexes(fieldDefinition) {
+      const amounts = orderBy(props.data.map((d, i) => [d[fieldDefinition.id].amount, i]), 0, 'desc');
+      return amounts.map(d => d[1]);
+    }
   }
 
   updateActiveFields(fieldType, fieldId) {
     this.setState((prevState) => {
       const { activeFields } = prevState;
-      activeFields[fieldType] = fieldId;
+      activeFields[fieldType] = this.availableFields[fieldType].find(d => d.id === fieldId);
       return { activeFields };
     });
   }
@@ -32,14 +42,17 @@ class Component extends React.Component {
     return (
       <div>
         <Nav
-          forestFields={this.forestFields}
-          financeFields={this.financeFields}
+          forestFields={this.availableFields.forest}
+          financeFields={this.availableFields.finance}
           updateActiveFields={this.updateActiveFields}
         />
         <Legend
-          forestFields={this.forestFields}
-          financeFields={this.financeFields}
           activeFields={this.state.activeFields}
+        />
+        <Table
+          data={this.props.data}
+          activeFields={this.state.activeFields}
+          limit="10"
         />
       </div>
     );
