952747508e4d458d2aa4adc41dab58a473c18c6a
Darren Anderson <dranders@colorado.ed>
2017-12-08 11:28:19 -0800

    Include a simple but working Table component to display data for the fields selected in Nav.

---
+++ src/components/table.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const debug = require('debug')('nydf:table');
+
+class TableColumn extends React.Component {
+  render() {
+    // debug(this.props.field.indexes);
+    let { indexes } = this.props.field;
+    if (this.props.limit) indexes = indexes.slice(0, this.props.limit);
+    return (
+      <div style={{ margin: '20px' }}>
+        <table><tbody>
+        {
+          indexes.map(i =>
+            <tr>
+              <td>{this.props.data[i].country}</td>
+              <td>{this.props.data[i][this.props.field.id].string}</td>
+            </tr>)
+        }
+        </tbody></table>
+      </div>
+    );
+  }
+}
+
+class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    // this.forestData = this.props.forestData;
+    // this.financeData = this.props.financeData;
+  }
+
+  render() {
+    // debug(this.props.data);
+    return (
+      <div className="panel panel--table">
+        <TableColumn
+          data={this.props.data}
+          field={this.props.activeFields.forest}
+          limit={this.props.limit}
+        />
+        <TableColumn
+          data={this.props.data}
+          field={this.props.activeFields.finance}
+          limit={this.props.limit}
+        />
+      </div>
+    );
+  }
+}
+
+export default Component;
