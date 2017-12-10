66a608de818beb067dd25ad8d109f114ecdfe0af
Darren Anderson <dranders@colorado.ed>
2017-12-10 14:32:16 -0800

    Implement bubbles for a fully functional map.

--- src/map.js
+++ src/map.js
@@ -41,20 +41,53 @@ function init(dataArg) {
 }
 
 function getBinColor(binPartitions, amount) {
-  // debug(binPartitions);
-  // debug(amount);
   const bin = binPartitions.find(partition => (amount <= partition.value));
   if (bin) return bin.color || 'cornsilk';
   return 'cornsilk';
 }
 
+function getBinSize(binPartitions, amount) {
+  const bin = binPartitions.find(partition => (amount <= partition.value));
+  if (bin) return bin.size || '10';
+  return '10';
+}
+
 function updateFields(fields) {
-  const areas = fields.forest.indexes.map((index) => {
-    const datum = data[index];
-    const amount = datum[fields.forest.id].amount;
-    return { id: datum.id, color: getBinColor(fields.forest.binPartitions, amount) };
+  const areas = [];
+  const images = [];
+  data.forEach((row) => {
+    const { id } = row;
+    const forestValue = row[fields.forest.id];
+    const financeValue = row[fields.finance.id];
+    const color = getBinColor(fields.forest.binPartitions, forestValue.amount);
+    const title = `${row.country}<br/>${forestValue.string} (${fields.forest.units})<br/>${financeValue.string} (${fields.finance.units})`;
+    const size = getBinSize(fields.finance.binPartitions, financeValue.amount);
+    areas.push({
+      id,
+      color,
+      title,
+    });
+    images.push({
+      id,
+      type: 'circle',
+      theme: 'light',
+      width: size,
+      height: size,
+      color: fields.finance.color,
+      labelColor: color,
+      labelRollOverColor: color,
+      borderColor: color,
+      outlineColor: '#ffffff',
+      outlineAlpha: 0.5,
+      outlineThickness: 2,
+      alpha: 0.5,
+      latitude: row.coordinates.latitude,
+      longitude: row.coordinates.longitude,
+      title,
+    });
   });
   map.dataProvider.areas = areas;
+  map.dataProvider.images = images;
   map.validateData();
 }
 
